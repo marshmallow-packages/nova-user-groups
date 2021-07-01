ac0739eaa1e7d43da379f999c1e8321294846d13
Stef van Esch <stef@marshmallow.dev>
2021-07-01 19:32:29 +0200

    icon update

--- src/Nova/UserGroup.php
+++ src/Nova/UserGroup.php
@@ -22,7 +22,7 @@ class UserGroup extends Resource
 
     public static $priority = 10;
 
-    public static $group_icon = '<svg xmlns="http://www.w3.org/2000/svg" class="sidebar-icon" enable-background="new 0 0 20 20" height="20" viewBox="0 0 20 20" width="20"><g><rect fill="none" height="20" width="20"/></g><g><path fill="var(--sidebar-icon)" d="M16,6h-4V4c0-0.55-0.45-1-1-1H9C8.45,3,8,3.45,8,4v2H4C3.45,6,3,6.45,3,7v9c0,0.55,0.45,1,1,1h12c0.55,0,1-0.45,1-1V7 C17,6.45,16.55,6,16,6z M9,4h2v4H9V4z M8,10c0.55,0,1,0.45,1,1s-0.45,1-1,1s-1-0.45-1-1S7.45,10,8,10z M10,14H6v-0.5 c0-0.67,1.33-1,2-1s2,0.33,2,1V14z M14,13h-3v-1h3V13z M14,11h-3v-1h3V11z"/></g></svg>';
+    public static $group_icon = '<svg xmlns="http://www.w3.org/2000/svg" class="sidebar-icon" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><rect fill="none" height="24" width="24"/><g><path fill="var(--sidebar-icon)" d="M12,12.75c1.63,0,3.07,0.39,4.24,0.9c1.08,0.48,1.76,1.56,1.76,2.73L18,18H6l0-1.61c0-1.18,0.68-2.26,1.76-2.73 C8.93,13.14,10.37,12.75,12,12.75z M4,13c1.1,0,2-0.9,2-2c0-1.1-0.9-2-2-2s-2,0.9-2,2C2,12.1,2.9,13,4,13z M5.13,14.1 C4.76,14.04,4.39,14,4,14c-0.99,0-1.93,0.21-2.78,0.58C0.48,14.9,0,15.62,0,16.43V18l4.5,0v-1.61C4.5,15.56,4.73,14.78,5.13,14.1z M20,13c1.1,0,2-0.9,2-2c0-1.1-0.9-2-2-2s-2,0.9-2,2C18,12.1,18.9,13,20,13z M24,16.43c0-0.81-0.48-1.53-1.22-1.85 C21.93,14.21,20.99,14,20,14c-0.39,0-0.76,0.04-1.13,0.1c0.4,0.68,0.63,1.46,0.63,2.29V18l4.5,0V16.43z M12,6c1.66,0,3,1.34,3,3 c0,1.66-1.34,3-3,3s-3-1.34-3-3C9,7.34,10.34,6,12,6z"/></g></svg>';
 
     /**
      * The model the resource corresponds to.
