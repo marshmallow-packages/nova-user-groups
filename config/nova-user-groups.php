6117b0b93cdca453a315398fb238d7ddc9437703
LTKort <lars.kort94@gmail.com>
2021-07-12 14:58:37 +0200

    WIP

--- config/nova-user-groups.php
+++ config/nova-user-groups.php
@@ -1,5 +1,28 @@
 <?php
 
 return [
-    //
+
+    /*
+    |--------------------------------------------------------------------------
+    | Allowed Methods
+    |--------------------------------------------------------------------------
+    |
+    | This is a list of methods the toolbar will not run on.
+    |
+    */
+    'groups' => [
+        'admin' => 'Administrator',
+        'super-admin' => 'SuperAdministrator',
+    ],
+    'methods' => [
+        'admin' => [
+            'viewNova',
+            'viewTelescope',
+            'viewHorizon'
+        ],
+        'super-admin' => [
+            'viewTelescope',
+            'viewHorizon'
+        ]
+    ],
 ];
--- src/Traits/HasUserGroup.php
+++ src/Traits/HasUserGroup.php
@@ -2,22 +2,40 @@
 
 namespace Marshmallow\NovaUserGroups\Traits;
 
+use Illuminate\Support\Arr;
 use Marshmallow\NovaUserGroups\NovaUserGroups;
+use Marshmallow\NovaUserGroups\Models\UserGroup;
 
 trait HasUserGroup
 {
+
     public function groups()
     {
         return $this->belongsToMany(NovaUserGroups::$userGroupModel);
     }
 
-    public function may($method, $resource_name, $arguments = null)
+    public function checkConfigMethod(UserGroup $group, string $method): bool
     {
-        if ($method == 'viewNova') {
-            return true;
+        $config_group = array_keys(config('nova-user-groups.groups'), $group->name, true);
+        if ($config_group = Arr::first($config_group)) {
+            $configMethod = "nova-user-groups.methods.{$config_group}";
+            if (in_array($method, config($configMethod))) {
+                return true;
+            }
         }
 
+        return false;
+    }
+
+
+    public function may($method, $resource_name, $arguments = null)
+    {
         foreach ($this->groups()->active()->get() as $group) {
+
+            if ($this->checkMethod($group, $method)) {
+                return true;
+            }
+
             foreach ($group->resources()->active()->get() as $resource) {
                 if ($resource->name == $resource_name) {
                     $action = $resource->actions()->active()->where('name', $method)->first();
